71262e98aa2db9f0e3034d7d83dc9eb7a763471f
Jared Odulio <jared.odulio@gmail.com>
2013-08-24 15:32:51 +0800

    initial commit

---
+++ fourgredients.js
@@ -0,0 +1,34 @@
+if (Meteor.isClient) {
+
+  Meteor.Router.add({
+    
+    '/': 'home',
+
+    '/about':  'about',  
+    
+    '/contact': 'contact', 
+    '/join': 'join',
+    '/help': 'help',
+    '/offer': 'offer'
+        
+  });
+
+  Template.home.greeting = function () {
+    return "Welcome to Fourgredients.";
+  };
+
+  Template.home.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+}
+
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
